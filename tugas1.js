afd6c7886f41aa50129916928f86aa717104b882
ajedkrap <mr.thedja@gmail.com>
2020-10-06 23:54:54 +0700

    submit

---
+++ tugas1.js
@@ -0,0 +1,13 @@
+function tugas1(){
+
+var namaDepan = "Dezza";
+let namaBelakang = "Rizqi";
+const umur = 25;
+
+console.log(namaDepan);
+console.log(namaBelakang);
+console.log(umur);
+
+}
+
+tugas1();
